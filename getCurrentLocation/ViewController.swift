06b794c19093ba8db614573e7b4c7ce1028de881
Mariem Ayadi <mayadi@smith.edu>
2014-11-09 14:43:48 -0500

    tell location manager to start updating location

--- getCurrentLocation/ViewController.swift
+++ getCurrentLocation/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    //NOTE:
+    //The CLLocationManagerDelegate protocol defines the methods used to receive location and heading updates from a CLLocationManager object.
 
+    let locationManager = CLLocationManager()
+    //NOTE:
+    //The CLLocationManager class is the central point for configuring the delivery of location- and heading-related events to your app. You use an instance of this class to establish the parameters that determine when location and heading events should be delivered and to start and stop the actual delivery of those events. You can also use a location manager object to retrieve the most recent location and heading data.
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // start updatug location once the application is loaded
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        //new for iOS8. We may add a description as to why we would like to add a user's description
+        self.locationManager.startUpdatingLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
